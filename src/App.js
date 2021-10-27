d9fd9ba5e283f0bfa3e605b173e04a4ce969f332
Nathan Duckett <nduckett13@gmail.com>
2021-10-27 20:50:18 +1300

    Add Formik and Material UI with examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage, FieldArray } from 'formik';
+
+const initialValues = {
+  friends: [
+    {
+      name: '',
+      email: '',
+    },
+  ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return (<div>
+    <h1>Invite friends</h1>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {({ values }) => (
+        <Form>
+          <FieldArray name="friends">
+            {({ insert, remove, push }) => (
+              <div>
+                {values.friends.length > 0 &&
+                  values.friends.map((friend, index) => (
+                    <div className="row" key={index}>
+                      <div className="col">
+                        <label htmlFor={`friends.${index}.name`}>Name</label>
+                        <Field
+                          name={`friends.${index}.name`}
+                          placeholder="Jane Doe"
+                          type="text"
+                        />
+                        <ErrorMessage
+                          name={`friends.${index}.name`}
+                          component="div"
+                          className="field-error"
+                        />
+                      </div>
+                      <div className="col">
+                        <label htmlFor={`friends.${index}.email`}>Email</label>
+                        <Field
+                          name={`friends.${index}.email`}
+                          placeholder="jane@acme.com"
+                          type="email"
+                        />
+                        <ErrorMessage
+                          name={`friends.${index}.name`}
+                          component="div"
+                          className="field-error"
+                        />
+                      </div>
+                      <div className="col">
+                        <button
+                          type="button"
+                          className="secondary"
+                          onClick={() => remove(index)}
+                        >
+                          X
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                <button
+                  type="button"
+                  className="secondary"
+                  onClick={() => push({ name: '', email: '' })}
+                >
+                  Add Friend
+                </button>
+              </div>
+            )}
+          </FieldArray>
+          <button type="submit">Invite</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+  )
+};
 
 export default App;
